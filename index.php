e9ef872104aaa0eab85fe0cf40c8c7c946492c89
tmdwns5422 <tmdwns5422@naver.com>
2016-09-01 10:43:45 +0900

    php 첫번째 수업 첫번째입니다.

--- index.php
+++ index.php
@@ -1,4 +1,24 @@
 <?php
     phpinfo();
+    // 이 부분은 주석입니다.
+    /*
+        이 부분은,
+        여러줄 주석입니다.
+    */
+    print "안녕하세요 저는 현승준 입니다.";
+    echo " 이걸 보게된뒤 당신은 5분후 사망합니다.";
+    // 이 부분은 출력부분 입니다.
 
+    print PHP_VERSION;
+    // 이 부분은 현재 사용하는 PHP의 버전을 출력해줍니다.
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
