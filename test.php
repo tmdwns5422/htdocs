b8272ee4fbcdcfea05efca989784385812570449
tmdwns5422 <tmdwns5422@naver.com>
2016-09-01 10:41:21 +0900

    php 첫번째 수업 두번째입니다.

--- test.php
+++ test.php
@@ -1,5 +1,5 @@
 <?php
-    phpinfo();
+    
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -15,8 +15,26 @@
         echo "<p>asd</p>";
         print "<p>내 옆에는 장애인이 있을지도 모릅니다!</p>";
     ?>
+    <li>3번째 예제<br>
+        <?php
+            print PHP_VERSION;
+            // 현재 PHP 버전을 나타냅니다.
+            print PHP_OS;
+            // 현재 OS 상태를 나타냅니다.
+            print __FILE__;
+            // PHP 현재 경로를 나타냅니다.
+        
+        
+            define("Hello","안녕하세요");
+            print Hello;
+            // define 에서 정의해준 Hello 를 출력하면 두번째 "안녕하세요"가 출력됨.
+            define("10","하이");
+            print 10;
+            // 하지만 상수는 안됨. 그대로 10 출력
+            
+        ?>
+    </li>
     <li>4번</li>
-    <li>5번</li>
 </ul>
 
     <header>a</header>
