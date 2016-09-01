d9397a156e09aac397edaa0b55d5a9f2f4059231
tmdwns5422 <tmdwns5422@naver.com>
2016-09-01 10:25:49 +0900

    php 첫번째 수업입니다.

---
+++ test.php
@@ -0,0 +1,26 @@
+<?php
+    phpinfo();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>test.php/index.php가 아닐경우 경로 설정이 달라짐</title>
+</head>
+<body>
+<ul>
+    <li>1번</li>
+    <li>2번</li>
+    <?php
+        echo "<p>asd</p>";
+        print "<p>내 옆에는 장애인이 있을지도 모릅니다!</p>";
+    ?>
+    <li>4번</li>
+    <li>5번</li>
+</ul>
+
+    <header>a</header>
+    <section>b</section>
+    <footer>c</footer>
+</body>
+</html>
