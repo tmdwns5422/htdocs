65d77857cf48273fe157913eab0e0d54920638a1
tmdwns5422 <tmdwns5422@naver.com>
2016-09-22 11:28:35 +0900

    kcc

--- list.php
+++ list.php
@@ -1,11 +1,11 @@
 <?php
     $search='%'.$_POST['search'].'%';
     include 'kccdb.php';
-    $sql = "select * from member where last_name like:last_name or first_name like:first_name";
+    $sql = "select * from member where last_name like :last_name or first_name like :first_name";
     $stmh = $pdo->prepare($sql); // 보안강화 sql injaection 으로부터
     $stmh->bindvalue(':last_name',$search,PDO::PARAM_STR);
     $stmh->bindvalue(':first_name',$search,PDO::PARAM_STR);
-    $stmh->execte();
+    $stmh->execute();
     $count = $stmh->rowcount();
     print $count."건입니다.";
     ?>
