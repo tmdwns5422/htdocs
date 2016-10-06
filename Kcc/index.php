affd3ce545e965ff37c0eb6b58b80b1dfa7c8c6f
tmdwns5422 <tmdwns5422@naver.com>
2016-10-06 11:31:59 +0900

    2016-10-06

--- Kcc/index.php
+++ Kcc/index.php
@@ -5,7 +5,7 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <link rel="stylesheet" href="./css/kcc.css">
-    <title>KCC</title>
+    <title>20626</title>
 </head>
 <body>
     <div id="wrap">
@@ -143,24 +143,24 @@
                             <td id="call">전화번호</td>
                             <td>
                             <select name="fN">
-                                <option value="1">02</option>
-                                <option value="2">031</option>
-                                <option value="3">032</option>
-                                <option value="4">033</option>
+                                <option value="02">02</option>
+                                <option value="031">031</option>
+                                <option value="032">032</option>
+                                <option value="033">033</option>
                             </select>&nbsp;-</td>
-                            <td><input type="text" name="sN" style="width:60px;">&nbsp;-</td>
-                            <td><input type="text" name="tN" style="width:60px;"></td>
+                            <td><input type="text" name="sN" style="width:60px;" maxlength="3">&nbsp;-</td>
+                            <td><input type="text" name="tN" style="width:60px;" maxlength="4"></td>
                             <td><span class="ex">&nbsp; 예약시 휴대폰으로 문자가 발송됩니다.</span></td>
                         </tr>
                         <tr>
                             <td id="cellphone">휴대폰번호&nbsp;<span class="star">*</span></td>
                              <td><select name="fPn">
-                                <option value="1">010</option>
-                                <option value="2">011</option>
-                                <option value="3">016</option>
-                                <option value="4">017</option>
-                                <option value="5">018</option>
-                                <option value="6">019</option>
+                                <option value="010">010</option>
+                                <option value="011">011</option>
+                                <option value="016">016</option>
+                                <option value="017">017</option>
+                                <option value="018">018</option>
+                                <option value="019">019</option>
                             </select>&nbsp;-</td>
                             <td>
                                 <input type="text" name="sPn" style="width:60px;" maxlength="4">&nbsp;-
@@ -183,14 +183,13 @@
                         </tr>            
                         <tr>
                             <td id="post">자택 우편번호&nbsp;<span class="star">*</span></td>
-                            <td><input type="text" name="fPostN" style="width:40px;margin-left:-40px;">&nbsp;-</td>
-                            <td><input type="text" name="sPostN" style="width:40px;"></td>
-                            <td><input type="button" value="우편번호 찾기"></td>
+                            <td><input type="text" id="postfive" name="fPostN" style="width:80px;margin-left:-40px;" placeholder="우편번호"></td>
+                            <td><input type="button" onclick="postCode()" value="우편번호 찾기"></td>
                         </tr>
                         <tr>
                             <td>자택주소&nbsp;<span class="star">*</span></td>
-                            <td><input type="text" name="address" style="width:300px;" ></td>
-                            <td><br><input type="text" name="Iaddress" style="width:300px; margin-left:131px;"></td>
+                            <td><input type="text" id="roadAddress" name="address" style="width:300px;" placeholder="도로명 주소" ></td>
+                            <td><br><input type="text" id="jibunAddress" name="Iaddress" style="width:300px; margin-left:131px;" placeholder="지번 주소"></td>
                         </tr>
                         <tr>
                             <td>DM발송처<span class="star">*</span></td>
@@ -220,14 +219,13 @@
                             </tr>
                             <tr>
                                 <td id="workpost">직장 우편번호<span class="star">*</span></td>
-                                <td><input type="text" name="efPostN" style="width:40px;">&nbsp;-</td>
-                                <td><input type="text" name="esPostN" style="width:40px;"></td>
-                                <td><input type="button" value="우편번호 찾기"></td>
+                                <td><input type="text" id="workpostfive" name="efPostN" style="width:80px;" placeholder="우편번호"></td>
+                                <td><input type="button" onclick="workpostCode()" value="우편번호 찾기"></td>
                             </tr>
                             <tr>
                                 <td>직장주소<span class="star">*</span></td>
-                                <td><input type="text" name="eAddress" style="width:300px; margin-left:4px;"></td>
-                                <td><br><input type="text" name="eIaddress" style="width:300px; margin-left:129px;"></td>
+                                <td><input type="text" id="eroadAddress" name="eAddress" style="width:300px; margin-left:4px;" placeholder="도로명 주소"></td>
+                                <td><br><input type="text" id="ejibunAddress" name="eIaddress" style="width:300px; margin-left:129px;" placeholder="지번 주소"></td>
                             </tr>
                             <tr>
                                 <td id="number">직장전화번호<span class="star">*</span></td>
@@ -311,4 +309,108 @@
         </div>
     </div>
 
-</body></html>
+</body>
+<script src="http://dmaps.daum.net/map_js_init/postcode.v2.js"></script>
+<script>
+    //본 예제에서는 도로명 주소 표기 방식에 대한 법령에 따라, 내려오는 데이터를 조합하여 올바른 주소를 구성하는 방법을 설명합니다.
+    function postCode() {
+        new daum.Postcode({
+            oncomplete: function(data) {
+                // 팝업에서 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분.
+
+                // 도로명 주소의 노출 규칙에 따라 주소를 조합한다.
+                // 내려오는 변수가 값이 없는 경우엔 공백('')값을 가지므로, 이를 참고하여 분기 한다.
+                var fullRoadAddr = data.roadAddress; // 도로명 주소 변수
+                var extraRoadAddr = ''; // 도로명 조합형 주소 변수
+
+                // 법정동명이 있을 경우 추가한다. (법정리는 제외)
+                // 법정동의 경우 마지막 문자가 "동/로/가"로 끝난다.
+                if(data.bname !== '' && /[동|로|가]$/g.test(data.bname)){
+                    extraRoadAddr += data.bname;
+                }
+                // 건물명이 있고, 공동주택일 경우 추가한다.
+                if(data.buildingName !== '' && data.apartment === 'Y'){
+                   extraRoadAddr += (extraRoadAddr !== '' ? ', ' + data.buildingName : data.buildingName);
+                }
+                // 도로명, 지번 조합형 주소가 있을 경우, 괄호까지 추가한 최종 문자열을 만든다.
+                if(extraRoadAddr !== ''){
+                    extraRoadAddr = ' (' + extraRoadAddr + ')';
+                }
+                // 도로명, 지번 주소의 유무에 따라 해당 조합형 주소를 추가한다.
+                if(fullRoadAddr !== ''){
+                    fullRoadAddr += extraRoadAddr;
+                }
+
+                // 우편번호와 주소 정보를 해당 필드에 넣는다.
+                document.getElementById('postfive').value = data.zonecode; //5자리 새우편번호 사용
+                document.getElementById('roadAddress').value = fullRoadAddr;
+                document.getElementById('jibunAddress').value = data.jibunAddress;
+
+                // 사용자가 '선택 안함'을 클릭한 경우, 예상 주소라는 표시를 해준다.
+                if(data.autoRoadAddress) {
+                    //예상되는 도로명 주소에 조합형 주소를 추가한다.
+                    var expRoadAddr = data.autoRoadAddress + extraRoadAddr;
+                    document.getElementById('guide').innerHTML = '(예상 도로명 주소 : ' + expRoadAddr + ')';
+
+                } else if(data.autoJibunAddress) {
+                    var expJibunAddr = data.autoJibunAddress;
+                    document.getElementById('guide').innerHTML = '(예상 지번 주소 : ' + expJibunAddr + ')';
+
+                } else {
+                    document.getElementById('guide').innerHTML = '';
+                }
+            }
+        }).open();
+    }
+    // 직장주소
+        function workpostCode() {
+        new daum.Postcode({
+            oncomplete: function(data) {
+                // 팝업에서 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분.
+
+                // 도로명 주소의 노출 규칙에 따라 주소를 조합한다.
+                // 내려오는 변수가 값이 없는 경우엔 공백('')값을 가지므로, 이를 참고하여 분기 한다.
+                var fullRoadAddr = data.roadAddress; // 도로명 주소 변수
+                var extraRoadAddr = ''; // 도로명 조합형 주소 변수
+
+                // 법정동명이 있을 경우 추가한다. (법정리는 제외)
+                // 법정동의 경우 마지막 문자가 "동/로/가"로 끝난다.
+                if(data.bname !== '' && /[동|로|가]$/g.test(data.bname)){
+                    extraRoadAddr += data.bname;
+                }
+                // 건물명이 있고, 공동주택일 경우 추가한다.
+                if(data.buildingName !== '' && data.apartment === 'Y'){
+                   extraRoadAddr += (extraRoadAddr !== '' ? ', ' + data.buildingName : data.buildingName);
+                }
+                // 도로명, 지번 조합형 주소가 있을 경우, 괄호까지 추가한 최종 문자열을 만든다.
+                if(extraRoadAddr !== ''){
+                    extraRoadAddr = ' (' + extraRoadAddr + ')';
+                }
+                // 도로명, 지번 주소의 유무에 따라 해당 조합형 주소를 추가한다.
+                if(fullRoadAddr !== ''){
+                    fullRoadAddr += extraRoadAddr;
+                }
+
+                // 우편번호와 주소 정보를 해당 필드에 넣는다.
+                document.getElementById('workpostfive').value = data.zonecode; //5자리 새우편번호 사용
+                document.getElementById('eroadAddress').value = fullRoadAddr;
+                document.getElementById('ejibunAddress').value = data.jibunAddress;
+
+                // 사용자가 '선택 안함'을 클릭한 경우, 예상 주소라는 표시를 해준다.
+                if(data.autoRoadAddress) {
+                    //예상되는 도로명 주소에 조합형 주소를 추가한다.
+                    var expRoadAddr = data.autoRoadAddress + extraRoadAddr;
+                    document.getElementById('guide').innerHTML = '(예상 도로명 주소 : ' + expRoadAddr + ')';
+
+                } else if(data.autoJibunAddress) {
+                    var expJibunAddr = data.autoJibunAddress;
+                    document.getElementById('guide').innerHTML = '(예상 지번 주소 : ' + expJibunAddr + ')';
+
+                } else {
+                    document.getElementById('guide').innerHTML = '';
+                }
+            }
+        }).open();
+    }
+</script>
+</html>
--- Kcc/lib/insert.php
+++ Kcc/lib/insert.php
@@ -1,17 +1,16 @@
 <?php
 include 'kccdb.php';
 
-$homeNumber = $_POST["fN"].$_POST["sN"].$_POST["tN"];
-$phoneNumber = $_POST["fPn"].$_POST["sPn"].$_POST["tPn"];
-$postNumber = $_POST["fPostN"].$_POST["sPostN"];
-$address = $_POST["address"].$_POST["IAddress"];
-$ePostNumber = $_POST["efPostN"].$_POST["esPostN"];
-$eaddress = $_POST["eAddress"].$_POST["eIaddress"];
-$eNumber = $_POST["efN"].$_POST["esN"].$_POST["etN"];
-$faxNumber = $_POST["ffN"].$_POST["fsN"].$_POST["ftN"];
+//$sql = "INSERT INTO member (kName,eName,bYear,bMonth,bDay,id,pw,fN,fPn,sns,email,fPostN,address,dm,emp,op,sp,efPostN,eAddress,efN,ffN,wedding,wYear,wMonth,wDay) VALUES('{$_POST['kName']}','{$_POST['eName']}','{$_POST['bYear']}','{$_POST['bMonth']}','{$_POST['bDay']}','{$_POST['id']}','{$_POST['pw']}','{$homeNumber}','{$phoneNumber}','{$_POST['sns']}','{$_POST['email']}','{$postNumber}','{$address}','{$_POST['dm']}','{$_POST['emp']}','{$_POST['op']}','{$_POST['sp']}','{$ePostNumber}','{$eaddress}','{$eNumber}','{$faxNumber}','{$_POST['wedding']}','{$_POST['wYear']}','{$_POST['wMonth']}','{$_POST['wDay']}')";
 
-$sql = "INSERT INTO member (kName,eName,bYear,bMonth,bDay,id,pw,fN,fPn,sns,email,fPostN,address,dm,emp,op,sp,efPostN,eAddress,efN,ffN,wedding,wYear,wMonth,wDay) VALUES('{$_POST['kName']}','{$_POST['eName']}','{$_POST['bYear']}','{$_POST['bMonth']}','{$_POST['bDay']}','{$_POST['id']}','{$_POST['pw']}','{$homeNumber}','{$phoneNumber}','{$_POST['sns']}','{$_POST['email']}','{$postNumber}','{$address}','{$_POST['dm']}','{$_POST['emp']}','{$_POST['op']}','{$_POST['sp']}','{$ePostNumber}','{$eaddress}','{$eNumber}','{$faxNumber}','{$_POST['wedding']}','{$_POST['wYear']}','{$_POST['wMonth']}','{$_POST['wDay']}')";
-    //$sql = "INSERT INTO member (kName,eName,bYear) VALUES('$kName','$eName','$bYear')";
-    $result = $pdo -> query($sql);
+$phn = $_POST["fPn"].$_POST["sPn"].$_POST["tPn"];
+$phoneNumber = preg_replace("/(^02.{0}|^01.{1}|[0-9]{3})([0-9]+)([0-9]{4})/", "$1-$2-$3", $phn);
+
+$n = $_POST["fN"].$_POST["sN"].$_POST["tN"];
+$Number = preg_replace("/(^02.{0}|^01.{1}|[0-9]{3})([0-9]+)([0-9]{4})/", "$1-$2-$3", $n);
+
+$sql = "INSERT INTO member set kName='{$_POST['kName']}' ,eName='{$_POST["eName"]}',bYear='{$_POST["bYear"]}',bMonth='{$_POST['bMonth']}',bDay='{$_POST['bDay']}',id='{$_POST['id']}',pw='{$_POST['pw']}',fN='{$Number}',fPn='{$phoneNumber}',sns='{$_POST['sns']}',email='{$_POST['email']}',fPostN='{$_POST['fPostN']}',address='{$_POST['address']}',Iaddress='{$_POST['Iaddress']}',dm='{$_POST['dm']}',emp='{$_POST['emp']}',op='{$_POST['op']}',sp='{$_POST['sp']}',efPostN='{$_POST['efPostN']}',eAddress='{$_POST['eAddress']}',efN='{$_POST['efN']}',ffN='{$_POST['ffN']}',wedding='{$_POST['wedding']}',wYear='{$_POST['wYear']}',wMonth='{$_POST['wMonth']}',wDay='{$_POST['wDay']}'";
+    //$sql = "INSERT INTO member (kName,eName,bYear) VALUES('$kName','$eName','$bYear')"; //이방법은 쓰지말것
+    $go = $pdo ->query($sql);
     echo "<script>location.href='login_view.php';</script>"
 ?>
