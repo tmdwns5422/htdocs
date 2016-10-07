5ec307d2e05c868112355ad3988d324d96c34031
tmdwns5422 <tmdwns5422@naver.com>
2016-10-07 11:34:55 +0900

    2016-10-07 로그인페이지 아이디 중복체크 하던중

--- Kcc/member.sql
+++ Kcc/member.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- 생성 시간: 16-10-06 04:32
--- 서버 버전: 10.1.13-MariaDB
--- PHP 버전: 5.6.20
+-- 생성 시간: 16-10-07 04:33
+-- 서버 버전: 10.1.16-MariaDB
+-- PHP 버전: 5.6.24
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -48,6 +48,7 @@ CREATE TABLE `member` (
   `sp` varchar(100) NOT NULL,
   `efPostN` int(11) NOT NULL,
   `eAddress` varchar(100) NOT NULL,
+  `eIaddress` varchar(100) NOT NULL,
   `efN` varchar(20) NOT NULL,
   `ffN` varchar(20) NOT NULL,
   `wedding` varchar(100) NOT NULL,
@@ -55,6 +56,14 @@ CREATE TABLE `member` (
   `wMonth` int(11) NOT NULL,
   `wDay` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- 테이블의 덤프 데이터 `member`
+--
+
+INSERT INTO `member` (`idx`, `kName`, `eName`, `bYear`, `bMonth`, `bDay`, `id`, `pw`, `fN`, `fPn`, `sns`, `email`, `fPostN`, `address`, `Iaddress`, `dm`, `emp`, `op`, `sp`, `efPostN`, `eAddress`, `eIaddress`, `efN`, `ffN`, `wedding`, `wYear`, `wMonth`, `wDay`) VALUES
+(34, '', '', 1964, 1, 1, '22', '22', '02', '010', '예', '', 0, '', '', '자택', '', '', '', 0, '', '', '010', '010', '미혼', 1964, 1, 1),
+(35, '', '', 1964, 1, 1, 'tmdwns5422', '14214ko', '02', '010', '예', 'tmdwns5422@naver.com', 0, '', '', '자택', '', '', '', 0, '', '', '010', '010', '미혼', 1964, 1, 1);
 
 --
 -- 덤프된 테이블의 인덱스
@@ -74,7 +83,7 @@ ALTER TABLE `member`
 -- 테이블의 AUTO_INCREMENT `member`
 --
 ALTER TABLE `member`
-  MODIFY `idx` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `idx` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
